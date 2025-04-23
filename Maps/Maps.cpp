36f0a60cb283921e86a820986fb38620f21d3d50
Work <Work@MSI>
2025-04-23 12:43:46 -0500

    Add project files.

---
+++ Maps/Maps.cpp
@@ -0,0 +1,14 @@
+// Maps.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include<map> 
+
+int main()
+{
+	std::map<std::string, int> wordsMappedToTheirLengths; 
+
+
+
+}
