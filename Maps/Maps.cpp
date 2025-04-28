d39759056ec9d4d09b85d46e912b5a2aee16bc5d
Work <Work@MSI>
2025-04-28 13:07:30 -0500

    Added a few piddling examples of std::map on April 28

--- Maps/Maps.cpp
+++ Maps/Maps.cpp
@@ -5,14 +5,73 @@
 
 #include<map> 
 
-int main()
+#include<queue> 
+
+void demoTheOrderedNatureOfStdMap()
 {
 	std::map<std::string, int> wordsMappedToTheirLengths; //std::map inherits from tree!
 
+	wordsMappedToTheirLengths.insert({ "asdfasdZ", 8 });
+	wordsMappedToTheirLengths.emplace("abc123", 6); //NOTE the lack of {}!
 
-	wordsMappedToTheirLengths["alpha"] = 5; 
-	wordsMappedToTheirLengths["beta"] = 4;
-	//etc.
+	wordsMappedToTheirLengths.insert({ "beta", 4 });
+	wordsMappedToTheirLengths.insert({ "alpha", 5 });
+
+	wordsMappedToTheirLengths.insert({ "charlie", 7 });
+
+	//NOT the way to print a map!
+	//for (int i = 0; i < wordsMappedToTheirLengths.size(); ++i)
+	//	std::cout << wordsMappedToTheirLengths[i] << "\n";
+
+	//THIS is the way!
+	for (const auto& currentPair : wordsMappedToTheirLengths)
+		std::cout << currentPair.first << ", " << currentPair.second << "\n";
+
+}
+
+void demoSomeCoordinateMapThings()
+{
+	std::map<std::pair<float, float>, std::string> coordinatesToLocationNames =
+	{
+		{{-95.0f, +32.0f}, "Tyler, TX" },
+		{{0.0f, 0.0f}, "NULL island"}
+	};
+
+
+	std::cout << coordinatesToLocationNames.count({ -95.0f, +32.0f }) << "\n";
+
+	std::cout << coordinatesToLocationNames.count({ -9.0f, +3.0f }) << "\n";
+
+	//std::cout << coordinatesToLocationNames.at({ -95.0f, +32.0f }) << "\n";
+	//std::cout << coordinatesToLocationNames.at({ 0.0f, 0.0f }) << "\n";
+
+	//std::pair<float, float> pairToSearchMapFor = { 123.f, 456.f };
+
+	std::pair<float, float> pairToSearchMapFor = { 0.0f, 0.0f };
+
+
+	if (coordinatesToLocationNames.find(pairToSearchMapFor) !=
+		coordinatesToLocationNames.end())
+	{
+		std::cout << "The KEY supplied MAPS to the value: " <<
+			coordinatesToLocationNames.at(pairToSearchMapFor) << "\n";
+	}
+
+	else
+	{
+		std::cout << pairToSearchMapFor.first << ", " 
+			<< pairToSearchMapFor.second
+			<< "was not found\n";
+	}
+}
+
+
+int main()
+{
+
+	std::priority_queue<std::string> namesOfPeopleInLine; //pq uses a "heap"! (a special type of tree)
+
+	//call the functions above, if desired
 
 
 }
